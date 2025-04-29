f9de3d40c458ee3e5a458346ace04f473f57c173
Martin Bonari <asaasado253@gmail.com>
2025-04-29 01:31:59 -0300

    slide de noticias primitivo y componentes basicos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { Punto1Component } from './components/punto1/punto1.component';
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent },
+    {path: 'punto1', component: Punto1Component },
+    {path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: '**', redirectTo: 'home' } // Wildcard route for a 404 page
+];
