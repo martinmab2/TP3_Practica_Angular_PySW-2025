51e1ba800042c9d3cd06a04284a4b7a29b801b75
Martin Bonari <asaasado253@gmail.com>
2025-05-04 04:32:52 -0300

    array de productos y vista de los mismos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SectionEnlacesComponent } from './components/section-enlaces/section-enlaces.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SectionEnlacesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
