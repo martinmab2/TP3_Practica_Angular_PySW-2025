66b3077d388ac04500f5bb4954a92ba94d443669
Martin Alejandro Bonari <131064127+martinmab2@users.noreply.github.com>
2025-05-06 06:01:07 -0300

    Componente punto 3, commmonmodule y recursos

---
+++ src/app/components/punto3/punto3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-punto3',
+  imports: [CommonModule],
+  templateUrl: './punto3.component.html',
+  styleUrl: './punto3.component.css'
+})
+export class Punto3Component {
+
+}
