57df8dbd04861333db03fa661785b447b680646b
Martin Alejandro Bonari <131064127+martinmab2@users.noreply.github.com>
2025-05-06 15:41:48 -0300

    Servicio de Boleteria parcialmente completado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './components/home/home.component';
 import { Punto1Component } from './components/punto1/punto1.component';
 import { Punto2Component } from './components/punto2/punto2.component';
 import { Punto3Component } from './components/punto3/punto3.component';
+import { Punto4Component } from './components/punto4/punto4.component';
 export const routes: Routes = [
     {path: 'home', component: HomeComponent },
     {path: 'punto1', component: Punto1Component },
     {path: 'punto2', component: Punto2Component},
     {path: 'punto3', component: Punto3Component},
+    {path: 'punto4', component: Punto4Component},
     {path: '', redirectTo: 'home', pathMatch: 'full' },
     {path: '**', redirectTo: 'home' } // Wildcard route for a 404 page
 ];
